e9ce999de7fb8a77a77ce1c02a9d568f2941c7a7
solveoai <solveoai7@gmail.com>
2025-07-21 14:15:01 -0400

    Updated LoadingScreen.tsx

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -23,7 +23,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ onComplete }) => {
 
     const timer3 = setTimeout(() => {
       setAnimationPhase('fadeOut');
-    }, 4800);
+    }, 1000);
 
     const timer4 = setTimeout(() => {
       onComplete();
