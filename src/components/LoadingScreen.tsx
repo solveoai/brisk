4ef55a4e8abb61d363b600c724d146b7acbd3d07
solveoai <solveoai7@gmail.com>
2025-07-21 14:31:15 -0400

    Fix logo to go to exact top-left navigation position

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -33,9 +33,9 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ onComplete }) => {
     logoVisible ? 'opacity-100' : 'opacity-0',
     phase === 'fadeIn' && 'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-[3]',
     phase === 'moveToHeader' &&
-      'top-4 left-4 md:left-8 lg:left-12 transform translate-x-0 translate-y-0 scale-100',
+      'top-4 left-4 md:top-4 md:left-8 lg:top-4 lg:left-12 transform translate-x-0 translate-y-0 scale-100',
     phase === 'fadeOut' &&
-      'top-4 left-4 md:left-8 lg:left-12 transform translate-x-0 translate-y-0 scale-100 opacity-0',
+      'top-4 left-4 md:top-4 md:left-8 lg:top-4 lg:left-12 transform translate-x-0 translate-y-0 scale-100 opacity-0',
   ]
     .filter(Boolean)
     .join(' ');
@@ -48,7 +48,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ onComplete }) => {
       {/* logo wrapper */}
       <div className={containerClasses}>
         <Logo
-          className="w-32 h-auto"                       /* final logo size */
+          className="w-48 h-12"                       /* final logo size matches navigation */
           variant="light"
           animated={logoVisible && phase === 'fadeIn'}   /* blade animation only during fadeIn */
         />
